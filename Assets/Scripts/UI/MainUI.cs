b08f87c07f05fb1f12bfae127d57fae15ea48ff8
Lenvear <guy25460@gmail.com>
2024-04-30 16:54:50 +0700

    Commit Week 1-2 Setup

--- Assets/Scripts/UI/MainUI.cs
+++ Assets/Scripts/UI/MainUI.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class MainUI : MonoBehaviour
 {
+    [SerializeField] 
+    private GameObject selectionMarker;
+    public GameObject SelectionMarker {get { return selectionMarker; } }
+    
     public static MainUI instance;
-
-    private void Awake()
+    void Awake()
     {
         instance = this;
-    }
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
