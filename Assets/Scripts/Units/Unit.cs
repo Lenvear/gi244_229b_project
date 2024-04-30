d106757cbfb952ddf2a55db87544d4f80fcbfc12
Lenvear <guy25460@gmail.com>
2024-04-30 21:15:28 +0700

    Commit Week 5-6 Setup

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -11,6 +11,14 @@ public enum UnitState
     Die
 }
 
+[System.Serializable]
+public struct UnitCost
+{
+    public int food;
+    public int wood;
+    public int gold;
+    public int stone;
+}
 public class Unit : MonoBehaviour
 {
     
@@ -54,9 +62,25 @@ public class Unit : MonoBehaviour
     public NavMeshAgent NavAgent { get { return navAgent; } }
 
     [SerializeField] private Faction faction;
+    public Faction Faction { get { return faction; } set { faction = value; } }
 
     [SerializeField] private GameObject selectionVisual;
     public GameObject SelectionVisual { get { return selectionVisual; } }
+    
+    [SerializeField] private UnitCost unitCost;
+    public UnitCost UnitCost { get { return unitCost; } }
+
+    //time for increasing progress 1% for this unit, less is faster
+    [SerializeField] private float unitWaitTime = 0.1f;
+    public float UnitWaitTime { get { return unitWaitTime; } }
+
+
+    [SerializeField] private bool isBuilder;
+    public bool IsBuilder { get { return isBuilder; } set { isBuilder = value; } }
+
+    [SerializeField] private Builder builder;
+    public Builder Builder { get { return builder; } }
+
 
     void Awake()
     {
